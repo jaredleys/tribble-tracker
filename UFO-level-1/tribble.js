e904d33cb90cc3b582c3e18a5add66d7ff8ce974
jaredleys <jaredleys@gmail.com>
2021-06-19 00:44:10 -0400

    added to js

--- UFO-level-1/tribble.js
+++ UFO-level-1/tribble.js
@@ -1,1 +1,14 @@
 // Appending to table in html
+var tableBody = d3.select("tbody");
+
+data.forEach(report => {
+    var row = tableBody.append('tr');
+    row.append('td').text(report.datetime);
+    row.append('td').text(report.city);
+    row.append('td').text(report.state);
+    row.append('td').text(report.country);
+    row.append('td').text(report.shape);
+    row.append('td').text(report.durationMinutes);
+    row.append('td').text(report.comments);
+
+})
