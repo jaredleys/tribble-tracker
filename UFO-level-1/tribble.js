f1673d44918dc8610ca1380a5f68d9830e7835e1
jaredleys <jaredleys@gmail.com>
2021-06-20 14:08:25 -0400

    Added date filter

--- UFO-level-1/tribble.js
+++ UFO-level-1/tribble.js
@@ -25,3 +25,22 @@ uniqDates.forEach(date => {
     var form = d3.select("#form-select");
     form.append('option').attr('value', date).text(date)
 });
+
+var dateSelect = d3.select("#form-select");
+dateSelect.on("input", runEnter);
+
+function runEnter() {
+    d3.selectAll('tr').remove();
+    var inputValue = d3.select("#form-select").property("value");
+    var matchingDate = data.filter(report => report.datetime === inputValue);
+    matchingDate.forEach(report => {
+        var row = tableBody.append('tr');
+        row.append('td').text(report.datetime);
+        row.append('td').text(report.city);
+        row.append('td').text(report.state);
+        row.append('td').text(report.country);
+        row.append('td').text(report.shape);
+        row.append('td').text(report.durationMinutes);
+        row.append('td').text(report.comments);
+});
+};
